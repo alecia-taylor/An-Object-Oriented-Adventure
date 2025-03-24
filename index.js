e4a00d117802e4f3f426f42abd4515caf5723290
A. Taylor <alecia.taylor0@gmail.com>
2025-03-24 04:00:34 +0000

    move roll method to the adventurer object for better structure

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const adventurer = {
             name: "Frank",
             type: "Flea",
             belongings:["hat","sunglasses","breif case"],//object inside an object, inside an object
-
-             roll (mod = 0) {
-                const result = Math.floor(Math.random() * 20) + 1 + mod;
-                console.log(`${this.name} rolled a ${result}.`) //method for "dice rolls
-                }
         }
+    },
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`) //method for "dice roll
     }
 }
+             
 
     for (const item of adventurer.inventory) {
         console.log(item);

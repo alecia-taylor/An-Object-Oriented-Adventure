2f017e25a54f7f50f29e7d8eb9b182c6c4fd8648
A. Taylor <alecia.taylor0@gmail.com>
2025-03-24 04:21:07 +0000

    Refactor Character class formatting and improve code readability

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ const adventurer = {
     } //Practice loop
       console.log(adventurer.roll()); //Practice method
 
-// Part 2 - Class Fantasy
-class Character {
+ // Part 2 - Class Fantasy
+ class Character {
     constructor (name) {
       this.name = name;
       this.health = 100;
@@ -35,12 +35,13 @@ class Character {
         console.log(`${this.name} rolled a ${result}.`) 
     }
   }
-  const robin = new Character("Robin");
-  robin.inventory = ["sword", "potion", "artifact"];
-  robin.companion = new Character("Leo");
-  robin.companion.type = "Cat";
-  robin.companion.companion = new Character("Frank");
-  robin.companion.companion.type = "Flea";
-  robin.companion.companion.inventory = ["small hat", "sunglasses", "briefcase"];
-    console.log(robin); 
+     const robin = new Character("Robin");
+     robin.inventory = ["sword", "potion", "artifact"];
+     robin.companion = new Character("Leo");
+     robin.companion.type = "Cat";
+     robin.companion.companion = new Character("Frank");
+     robin.companion.companion.type = "Flea";
+     robin.companion.companion.inventory = ["small hat", "sunglasses", "briefcase"];
+         console.log(robin); 
 
+//Part 3 - Class Features

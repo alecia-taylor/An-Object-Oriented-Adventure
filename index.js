6e42728a3d942878961b3f436c7f06a56623c490
A. Taylor <alecia.taylor0@gmail.com>
2025-03-24 04:11:09 +0000

    add Character class with roll method for dice rolling functionality

--- index.js
+++ index.js
@@ -22,5 +22,16 @@ const adventurer = {
         console.log(item);
     } //Practice loop
       console.log(adventurer.roll()); //Practice method
-      
-// Part 2 - Nested Companions
+
+// Part 2 - Class Fantasy
+class Character {
+    constructor (name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+    }
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`) //method for "dice roll
+    }
+  }

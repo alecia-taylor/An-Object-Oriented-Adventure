413f1e39aaa36c49097682580479065bc7e7e7d7
A. Taylor <alecia.taylor0@gmail.com>
2025-03-24 03:44:19 +0000

    add roll method to companion property of Adventurer object

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const adventurer = {
         companion: {
             name: "Frank",
             type: "Flea",
-            belongings:["hat","sunglasses","breif case"]
+            belongings:["hat","sunglasses","breif case"],
+
+             roll (mod = 0) {
+                const result = Math.floor(Math.random() * 20) + 1 + mod;
+                console.log(`${this.name} rolled a ${result}.`)
+                }
         }
     }
 }
